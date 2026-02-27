c14982fda2a769b8595634b8d3123b45f7de7675
elvis262 <angelvisalui@gmail.com>
2026-02-27 03:30:54 +0000

    third commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,19 +23,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/logo.jpeg',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/logo.jpeg',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/logo.jpeg',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/logo.jpeg',
   },
 }
 
